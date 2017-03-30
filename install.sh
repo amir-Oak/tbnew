f1330d3a836f20b540df7809c7dbabfae8bbe149
botiiing <sddsfv85df4@gmail.com>
2017-03-30 11:47:09 +0430

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,12 @@
 wget "https://valtman.name/files/telegram-cli-1222"
 sudo apt-get -y install libreadline6 libreadline-dev libreadline-dev libreadline6-dev libconfig-dev libssl-dev tmux lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip redis-server autoconf git g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev software-properties-common python-software-properties gcc-6
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+sudo apt-get install lua50
+sudo apt-get install lua5.1
+sudo apt-get install lua5.2
+sudo apt-get install lua5.3
+sudo apt-get autoclean
+sudo apt-get autoremove
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get dist-upgrade
