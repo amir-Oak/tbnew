c2b68223e8861289dbadafb963d820f0e47c7606
botiiing <sddsfv85df4@gmail.com>
2017-03-30 11:38:08 +0430

    Add files via upload

---
+++ cleaner.lua
@@ -0,0 +1,7 @@
+redis = (loadfile "redis.lua")()
+io.write("Enter Tabchi ID : ")
+local last = io.read()
+io.popen('rm -rf ~/.telegram-cli/tabchi-'..last..' tabchi-'..last..'.lua tabchi-'..last..'.sh tabchi_'..last..'_logs.txt')
+redis:del('tabchi:'..last..':*')
+print("Done!\nAll Data/Files Of Tabchi Deleted\nTabchi ID : "..last)
+print("Version 4.3 Of Tabchi")
